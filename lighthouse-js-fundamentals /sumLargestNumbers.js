ffc5f93f2709064eab866927cd7fcd3a04087c49
quinjenn <jquintal_@hotmail.com>
2023-01-06 16:05:33 -0800

    <adding files to repo>

---
+++ lighthouse-js-fundamentals /sumLargestNumbers.js
@@ -0,0 +1,8 @@
+const sumLargestNumbers = function (data) {
+  // Put your solution here
+  let highest = Math.max(data[0], data[1]);
+  let secondHighest = Math.min(data[0], data[1]);
+};
+console.log(sumLargestNumbers([1, 10]));
+console.log(sumLargestNumbers([1, 2, 3]));
+console.log(sumLargestNumbers([10, 4, 34, 6, 92, 2]));
